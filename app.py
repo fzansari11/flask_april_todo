d1a8b41c863ceef484fa3f301ea981cd8874da1c
Zee <fzansari11@gmail.com>
2025-05-09 19:21:15 -0400

    Change

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ db_pass = os.environ.get('DB_PASS')
 db_host = os.environ.get('DB_HOST')
 db_name = os.environ.get('DB_NAME')
 
+# Set up AWS credentials
 def get_db_secret(secret_name, region_name='us-east-2'):
     client = boto3.client('secretsmanager', region_name=region_name)
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
